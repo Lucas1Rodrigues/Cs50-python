96f16dbb2884fec4ce813eb3a1eac70bac89ee9c
Lucas Christian <lucasrodriguezy@gmail.com>
2026-09-03 18:00:32 -0300

    files

--- files/exemples.py
+++ files/exemples.py
@@ -1,3 +1,6 @@
-with open("text.txt",'r') as f:
-   for line in f:
-    print(line, end='')
+#aqui sobrescreveu o artigo existente
+with open("text2.txt","w",encoding="UTF-8") as f:
+    f_write = f.write("Teste, aqui eu escrevi um texto.")
+#escreve no final do arquivo
+with open("text2.txt","a",encoding="UTF-8") as f:
+    f_write = f.write("Teste, aqui eu escrevi um texto.")
